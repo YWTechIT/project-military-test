247cdc78fd7fcf64b8f6cf2ea615f0d51d4434bc
YWTechIT <abcd8637@naver.com>
2021-05-11 21:06:18 +0900

    Create index.js

---
+++ src/components/Answer/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const AnswerStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 8px;
+`;
+
+export default AnswerStyle;
