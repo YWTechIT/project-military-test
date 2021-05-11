1fb5f58309b0b731c5b2b158fb8c19d901d73615
YWTechIT <abcd8637@naver.com>
2021-05-11 21:06:34 +0900

    Create Constant.js

---
+++ src/pages/Combat/Constant.js
@@ -0,0 +1,89 @@
+const COMBAT_QUIZ = [
+	{
+		id: 1,
+		question: "나는 마음만 먹으면 뭐든 할 수 있다고 생각한다.",
+		answers: [
+			{ text: "그렇다.", short: 10, long: 0 },
+			{ text: "아니다.", short: 0, long: 10 },
+		],
+	},
+	{
+		id: 2,
+		question: "나는 전략게임(StarCraft, WarCraft, etc..)을 할 때 일꾼과 같은 기본적인 유닛을 많이 생산하는 편이다.",
+		answers: [
+			{ text: "그렇다.", short: 0, long: 10 },
+			{ text: "아니다.", short: 10, long: 0 },
+		],
+	},
+	{
+		id: 3,
+		question: "개인정비시간에 맞선임이 물건을 옮기고 있는 모습을 봤을 때 당신이 드는 생각은?",
+		answers: [
+			{ text: "(하던일을 제쳐두고) ' 제가 들겠습니다! '라고 말한다.", short: 10, long: 0},
+			{ text: "이것만 끝내고 도와줘야지라고 속으로 생각한다.", short: 5, long: 0 },
+			{ text: "나한테 시킨일이 아니니까 모른척한다.", short: 0, long: 10},
+			{ text: "어제 다 못들은 후임 연애 썰 들으러 간다.", short: 0, long: 5},
+		],
+	},
+	{
+		id: 4,
+		question: "나는 주로 아날로그보다 디지털을 선호하는 편이다.",
+		answers: [
+			{ text: "그렇다.", short: 10, long: 0 },
+			{ text: "아니다.", short: 0, long: 10 },
+		],
+	},
+	{
+		id: 5,
+		question: "40km 행군 도중 ' 저기 저 산만 올라면 도착이다. '라는 말을 2시간째 듣고있는 당신의 속마음은? ",
+		answers: [
+			{ text: "' 저 산만 올라가면 도착이겠지?! ' 하며 끝까지 힘을 낸다.", short: 5, long: 0 },
+			{ text: "대체 왜 이런 코스로 가는거야?! 하며 투덜댄다.", short: 0, long: 5 },
+			{ text: "동기들도 똑같이 힘드니까 괜찮아 하며 위로한다.", short: 5, long: 5 },
+			{ text: "'소대장님, 진짜 XX 너무 힘듭니다. 제 군장 좀 들어주십쇼' 라고 얘기한다.", short: 0, long: 5 },
+
+		],
+    },
+    {
+		id: 6,
+		question: "나에게 능력이 하나 주어진다면?!",
+		answers: [
+			{ text: "상대방을 설득하는 능력", short: 20, long: 0 },
+			{ text: "수학적인 계산에 탁월한 능력", short: 20, long: 20 },
+			{ text: "무한 체력", short: 30, long: 0 },
+            { text: "설계도만 있으면 무엇이든 지을 수 있는 능력", short: 5, long: 0 },
+            { text: "기계를 능숙하게 다룰 수 있는 능력", short: 0, long: 20 },
+		],
+	},
+	{
+		id: 7,
+		question: '오늘은 전술평가가 있는 날이다. 출동준비를 마치고 잠깐 후임과 쉬고있던 나. 문득 어제 불침번근무 중 무전기 배터리를 충전하지 않은 것이 떠오르고 이 사실을 나만 알고있는데...?',
+		answers: [
+			{ text: "허겁지겁 분대장에게 달려가 배터리가 방전됐다고 보고한다.", short: 5, long: 0 },
+			{ text: "인생사 어쩔 수 없지. 그냥 모른체 한다.", short: 0, long: 5 },
+			{ text: "후임을 시켜 분대장에게 보고하라고 한다.", short: 0, long: 5 },
+			{ text: "나보다 짬이 낮은 옆 분대 무전병의 배터리를 몰래 가져온다.", short: 0, long: 5 },
+		],
+	},
+	
+	{
+		id: 8,
+		question: "친구와 약속장소에서 보기로 했는데, 약속장소까지 어떻게 갈것인가?",
+		answers: [
+			{ text: "걸어서 간다.", short: 5, long: 0 },
+			{ text: "내일 만나자고 한다.", short: 0, long: 5 },
+			{ text: "대중교통을 타고 간다.", short: 5, long: 5 },
+            { text: "전동퀵보드를 빌려 타고간다.", short: 0, long: 5 },
+		],
+	},
+	{
+		id: 9,
+		question: "나는 겉으로 보여지는 것보다, 보이지 않는 곳에 신경이 더 쓰인다.",
+		answers: [
+			{ text: "그렇다.", short: 10, long: 0 },
+			{ text: "아니다.", short: 0, long: 10 },
+		],
+	},
+];
+
+export { COMBAT_QUIZ };
