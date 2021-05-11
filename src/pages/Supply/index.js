958f774fce719063d7d98c5115fc5342d36d0ad7
YWTechIT <abcd8637@naver.com>
2021-05-11 21:06:40 +0900

    Create index.js

---
+++ src/pages/Supply/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import SupplyAnswer from "../../components/Answer/Supply";
+import Container from "../../components/Container";
+import Question from "../../components/Question";
+import { SUPPLY_QUIZ } from "./Constant";
+
+const Supply = ({ score, setScore }) => {
+  let history = useHistory();
+  const [supplyCurrentNo, setSupplyCurrentNo] = useState(0);
+
+  const supplyHandleClick = (affair, medic, weapon, police, pray, band) => {
+    if (supplyHandleClick) {
+      setScore((score) => ({
+        ...score,
+        supply: {
+          affairScore: score.supply.affairScore + affair,
+          medicScore: score.supply.medicScore + medic,
+          weaponScore: score.supply.weaponScore + weapon,
+          policeScore: score.supply.policeScore + police,
+          prayScore: score.supply.prayScore + pray,
+          bandScore: score.supply.bandScore + band,
+        },
+      }));
+    }
+
+    if (supplyCurrentNo === SUPPLY_QUIZ.length - 1) {
+      history.push("/loading");
+    } else {
+      setSupplyCurrentNo((supplyCurrentNo) => supplyCurrentNo + 1);
+    }
+  };
+
+  console.log(score.supply.affairScore, score.supply.medicScore, score.supply.weaponScore, score.supply.policeScore, score.supply.prayScore, score.supply.bandScore);
+
+  return (
+    <Container>
+      <Question QUIZZES={SUPPLY_QUIZ} currentNo={supplyCurrentNo}></Question>
+      <SupplyAnswer
+        QUIZZES={SUPPLY_QUIZ}
+        currentNo={supplyCurrentNo}
+        handleClick={supplyHandleClick}
+      ></SupplyAnswer>
+    </Container>
+  );
+};
+
+export default Supply;
