8f5181a2c7e47b683a1ddca7931272ec39ed8b57
YWTechIT <abcd8637@naver.com>
2021-05-15 15:24:34 +0900

    Refactor Supply Page HandleClick function

--- src/pages/Supply/index.js
+++ src/pages/Supply/index.js
@@ -1,44 +1,49 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import SupplyAnswer from "../../components/Answer/Supply";
+import AnswerGroup from "../../components/AnswerGroup";
 import Container from "../../components/Container";
 import Question from "../../components/Question";
 import { SUPPLY_QUIZ } from "./Constant";
 
 const Supply = ({ score, setScore }) => {
   let history = useHistory();
-  const [supplyCurrentNo, setSupplyCurrentNo] = useState(0);
+  const [currentNo, setCurrentNo] = useState(0);
 
-  const supplyHandleClick = (affair, medic, weapon, police, pray, band) => {
-    if (supplyHandleClick) {
-      setScore((score) => ({
-        ...score,
-        supply: {
-          affairScore: score.supply.affairScore + affair,
-          medicScore: score.supply.medicScore + medic,
-          weaponScore: score.supply.weaponScore + weapon,
-          policeScore: score.supply.policeScore + police,
-          prayScore: score.supply.prayScore + pray,
-          bandScore: score.supply.bandScore + band,
-        },
-      }));
-    }
+  const supplyScore = (answer) => {
+    const affair = answer.affair;
+    const medic = answer.medic;
+    const weapon = answer.weapon;
+    const police = answer.police;
+    const pray = answer.pray;
+    const band = answer.band;
 
-    if (supplyCurrentNo === SUPPLY_QUIZ.length - 1) {
+    setScore((score) => ({
+      ...score,
+      supply: {
+        affairScore: score.supply.affairScore + affair,
+        medicScore: score.supply.medicScore + medic,
+        weaponScore: score.supply.weaponScore + weapon,
+        policeScore: score.supply.policeScore + police,
+        prayScore: score.supply.prayScore + pray,
+        bandScore: score.supply.bandScore + band,
+      },
+    }));
+
+    if (currentNo === SUPPLY_QUIZ.length - 1) {
       history.push("/loading");
     } else {
-      setSupplyCurrentNo((supplyCurrentNo) => supplyCurrentNo + 1);
+      setCurrentNo((currentNo) => currentNo + 1);
     }
   };
 
   return (
     <Container>
-      <Question QUIZZES={SUPPLY_QUIZ} currentNo={supplyCurrentNo}></Question>
-      <SupplyAnswer
+      <Question QUIZZES={SUPPLY_QUIZ} currentNo={currentNo}></Question>
+      <AnswerGroup
         QUIZZES={SUPPLY_QUIZ}
-        currentNo={supplyCurrentNo}
-        handleClick={supplyHandleClick}
-      ></SupplyAnswer>
+        currentNo={currentNo}
+        handleClick={supplyScore}
+      ></AnswerGroup>
     </Container>
   );
 };
