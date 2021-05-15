0dc69c71155e13362705708a06c4292405a74ce4
YWTechIT <abcd8637@naver.com>
2021-05-15 15:24:22 +0900

    Refactor Category Page HandleClick function

--- src/pages/Category/index.js
+++ src/pages/Category/index.js
@@ -1,42 +1,41 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import CategoryAnswer from "../../components/Answer/Category";
+import AnswerGroup from "../../components/AnswerGroup";
 import Container from "../../components/Container";
 import Question from "../../components/Question";
 import { CATEGORIZE_QUIZ } from "./Constant";
 
 const Category = ({ score, setScore }) => {
   let history = useHistory();
-  const [categoryCurrentNo, setCategoryCurrentNo] = useState(0);
+  const [currentNo, setCurrentNo] = useState(0);
 
-  const categoryHandleClick = (combat, supply) => {
-    if (categoryHandleClick) {
-      setScore((score) => ({
-        ...score,
-        category: {
-          combatScore: score.category.combatScore + combat,
-          supplyScore: score.category.supplyScore + supply,
-        },
-      }));
-    }
-    if (categoryCurrentNo === CATEGORIZE_QUIZ.length - 1) {
+  const categoryHandleClick = (answer) => {
+    const combat = answer.combat;
+    const supply = answer.supply;
+    setScore((score) => ({
+      ...score,
+      category: {
+        combatScore: score.category.combatScore + combat,
+        supplyScore: score.category.supplyScore + supply,
+      },
+    }));
+
+    if (currentNo === CATEGORIZE_QUIZ.length - 1) {
       history.push("/loading");
     } else {
-      setCategoryCurrentNo((categoryCurrentNo) => categoryCurrentNo + 1);
+      setCurrentNo((currentNo) => currentNo + 1);
     }
+    console.log(score.category.combatScore, score.category.supplyScore);
   };
-  
+
   return (
     <Container>
-      <Question
+      <Question QUIZZES={CATEGORIZE_QUIZ} currentNo={currentNo}></Question>
+      <AnswerGroup
         QUIZZES={CATEGORIZE_QUIZ}
-        currentNo={categoryCurrentNo}
-      ></Question>
-      <CategoryAnswer
-        QUIZZES={CATEGORIZE_QUIZ}
-        currentNo={categoryCurrentNo}
+        currentNo={currentNo}
         handleClick={categoryHandleClick}
-      ></CategoryAnswer>
+      ></AnswerGroup>
     </Container>
   );
 };
