06972ae2da4331036a31a05452a6fae928dd87bf
YWTechIT <abcd8637@naver.com>
2021-05-20 21:49:48 +0900

    Update Category question / answers

--- src/pages/Category/Constant.js
+++ src/pages/Category/Constant.js
@@ -5,20 +5,20 @@ const CATEGORIZE_QUIZ = [
 		question: "친구들과 5:5 LOL을 하기로 했다. 내가 가고 싶은 라인은? (실력은 동일)",
 		answers: [
 			{ text: "탑", combat: 2, supply: 0 },
+			{ text: "정글", combat: 1, supply: 0 },
 			{ text: "미드", combat: 2, supply: 0 },
 			{ text: "원딜", combat: 1, supply: 1 },
 			{ text: "서폿", combat: 0, supply: 2 },
-			{ text: "정글", combat: 1, supply: 0 },
 		],
 	},
 	{
 		id: 2,
-		question: "전쟁 중 적군과 1:1로 대치 중인 상황일때, 당신의 선택은?",
+		question: "친구들과 차를 타고 여행을 갈 때 나의 위치는?",
 		answers: [
-			{ text: "총알 3발 남은 K-2 소총", combat: 2, supply: 0 },
-			{ text: "녹슨 대검", combat: 2, supply: 0 },
-			{ text: "조용히 드러누워 시체인척 한다.", combat: 0, supply: 1 },
-			{ text: "지원요청을 한다.", combat: 0, supply: 2},
+			{ text: "운전석", combat: 2, supply: 0 },
+			{ text: "조수석", combat: 1, supply: 1 },
+			{ text: "운전석 뒤", combat: 0, supply: 2},
+			{ text: "조수석 뒤", combat: 1, supply: 1},
 		],
 	},
 	{
@@ -35,18 +35,19 @@ const CATEGORIZE_QUIZ = [
 		id: 4,
 		question: "친구와 식당에서 밥을 먹고있는데 직원이 실수로 물을 쏟았다. 이때, 당신의 행동은?",
 		answers: [
-			{ text: "화를 내며 당장 매니저를 불러달라고 한다.", combat: 2, supply: 0},
-			{ text: "괜찮다며 웃으며 넘긴다.", combat: 0, supply: 2},
-			{ text: "전화번호를 물어본다.", combat: 1, supply: 0},
-			{ text: "SNS에서 별점을 1개만 준다.", combat: 0, supply: 1},
+			{ text: "화를 내며 당장 사장을 불러달라고 한다.", combat: 2, supply: 0},
+			{ text: "괜찮다며 웃으며 넘긴다.", combat: 1, supply: 1},
+			{ text: "주문을 취소하고 식당을 나간다.", combat: 1, supply: 0},
+			{ text: "SNS에서 별점을 1개만 준다.", combat: 1, supply: 2},
 		],
 	},
 	{
 		id: 5,
-		question: "나는 단순하고 명확한 것을 좋아한다.",
+		question: "지나가던 골목에서 불량배가 어린아이의 돈을 뺐는 모습을 보았다. 이때, 당신의 행동은?",
 		answers: [
-			{ text: "O", combat: 1, supply: 0 },
-			{ text: "X", combat: 0, supply: 1 },
+			{ text: "직접 가서 도와준다.", combat: 2, supply: 0 },
+			{ text: "경찰에 신고 후 올때까지 기다린다.", combat: 0, supply: 2 },
+			{ text: "그냥 지나간다.", combat: 1, supply: 1 },
 		],
 	},
 ];
