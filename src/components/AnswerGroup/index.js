cd2b0c820af79d146de77775bc1d50bbc5dd3b23
YWTechIT <abcd8637@naver.com>
2021-05-21 05:50:10 +0900

    Fix key warning in map function

--- src/components/AnswerGroup/index.js
+++ src/components/AnswerGroup/index.js
@@ -1,20 +1,21 @@
 import React from "react";
 import Button from "../Button";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const AnswerGroupWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 8px;
 `;
 
-const AnswerGroup = ({QUIZZES, currentNo, handleClick}) => {
+const AnswerGroup = ({ QUIZZES, currentNo, handleClick }) => {
   return (
     <AnswerGroupWrapper>
       {QUIZZES[currentNo].answers.map((answer) => (
         <Button
+          key={answer.text}
           text={answer.text}
           onClick={() => handleClick(answer)}
         ></Button>
@@ -24,4 +25,3 @@ const AnswerGroup = ({QUIZZES, currentNo, handleClick}) => {
 };
 
 export default AnswerGroup;
-
