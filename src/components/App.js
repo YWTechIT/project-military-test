139eee7e0221b31a56856096df0a628e174d5d8c
YWTechIT <abcd8637@naver.com>
2021-05-21 14:08:14 +0900

    Move defaultScore object App.js to components/defaultScore/index.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,40 +5,21 @@ import Category from "../pages/Category";
 import Combat from "../pages/Combat";
 import Supply from "../pages/Supply";
 import theme from "../theme";
+import defaultScore from "./defaultScore";
+import getCode from "./getCode";
 
 const App = () => {
-  const defaultScore = {
-    category: {
-      combatScore: 0,
-      supplyScore: 0,
-    },
-    combat: {
-      infantryScore: 0,
-      artilleryScore: 0,
-      armorScore: 0,
-      engineerScore: 0,
-      signalScore: 0,
-      intelligenceScore: 0,
-    },
-    supply: {
-      affairScore: 0,
-      medicScore: 0,
-      weaponScore: 0,
-      policeScore: 0,
-      prayScore: 0,
-      bandScore: 0,
-    },
-  };
-
   const [score, setScore] = useState(defaultScore);
+  const getCombatCode = getCode(score.combat);
+  const getSupplyCode = getCode(score.supply);
 
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <Category score = {score} setScore={setScore}></Category>
-        <Combat score = {score} setScore={setScore}></Combat>
-        <Supply score = {score} setScore={setScore}></Supply>
+        <Category score={score} setScore={setScore}></Category>
+        <Combat score={score} setScore={setScore}></Combat>
+        <Supply score={score} setScore={setScore}></Supply>
       </ThemeProvider>
     </>
   );
