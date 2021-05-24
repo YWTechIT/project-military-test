402e41a36ab9d3068740aa8b3c3a4d104e0e87b3
YWTechIT <abcd8637@naver.com>
2021-05-24 21:41:46 +0900

    Update category, combat, supply questions
    카테고리, 전투, 비전투 문제 변경

--- src/pages/Category/Constant.js
+++ src/pages/Category/Constant.js
@@ -2,23 +2,21 @@
 const CATEGORIZE_QUIZ = [
 	{
 		id: 1,
-		question: "친구들과 5:5 LOL을 하기로 했다. 내가 가고 싶은 라인은? (실력은 동일)",
+		question: "친구들과 5:5 LOL을 하기로 했다. 내가 선택하고 싶은 라인은? (실력은 동일)",
 		answers: [
 			{ text: "탑", combat: 2, supply: 0 },
-			{ text: "정글", combat: 1, supply: 0 },
-			{ text: "미드", combat: 2, supply: 0 },
+			{ text: "정글", combat: 1, supply: 1 },
+			{ text: "미드", combat: 1, supply: 1 },
 			{ text: "원딜", combat: 1, supply: 1 },
 			{ text: "서폿", combat: 0, supply: 2 },
 		],
 	},
 	{
 		id: 2,
-		question: "친구들과 차를 타고 여행을 갈 때 나의 위치는?",
+		question: "한라산 등반 도중 중간 지점까지 도착했는데 체력이 고갈된 상태다. 이때 당신의 선택은?",
 		answers: [
-			{ text: "운전석", combat: 2, supply: 0 },
-			{ text: "조수석", combat: 1, supply: 1 },
-			{ text: "운전석 뒤", combat: 0, supply: 2},
-			{ text: "조수석 뒤", combat: 1, supply: 1},
+			{ text: "여기까지 올라왔는데 정상은 찍어야지!", combat: 3, supply: 0 },
+			{ text: "여기까지 올라온것도 대단한데 그만 내려가자.", combat: 0, supply: 3 },
 		],
 	},
 	{
@@ -26,27 +24,27 @@ const CATEGORIZE_QUIZ = [
 		question: "조별과제 중 첫눈에 반한 이성이 있을 때 당신의 행동은?",
 		answers: [
 			{ text: "그(그녀)의 번호를 물어본다.", combat: 2, supply: 0 },
-			{ text: "이성의 SNS를 들어가본다.", combat: 0, supply: 1 },
-			{ text: "내 주변에 그(그녀)와 아는사람이 있는지 물어본다.", combat: 1, supply: 2},
-			{ text: "인연이라면 언젠간 또 보겠지하며 무심코 지나간다.", combat: 2, supply: 1},
+			{ text: "이성의 SNS를 들어가본다.", combat: 0, supply: 2 },
+			{ text: "내 주변에 그(그녀)와 아는사람이 있는지 물어본다.", combat: 1, supply: 1},
+			{ text: "인연이라면 언젠간 또 보겠지하며 무심코 지나간다.", combat: 1, supply: 1},
 		],
 	},
 	{
 		id: 4,
-		question: "친구와 식당에서 밥을 먹고있는데 직원이 실수로 물을 쏟았다. 이때, 당신의 행동은?",
+		question: "친구와 식당에서 밥을 먹고있는데 직원이 실수로 물을 쏟았다. 이때 당신의 행동은?",
 		answers: [
 			{ text: "화를 내며 당장 사장을 불러달라고 한다.", combat: 2, supply: 0},
 			{ text: "괜찮다며 웃으며 넘긴다.", combat: 1, supply: 1},
-			{ text: "주문을 취소하고 식당을 나간다.", combat: 1, supply: 0},
-			{ text: "SNS에서 별점을 1개만 준다.", combat: 1, supply: 2},
+			{ text: "주문을 취소하고 식당을 나간다.", combat: 1, supply: 1},
+			{ text: "SNS에서 별점을 1개만 준다.", combat: 0, supply: 2},
 		],
 	},
 	{
 		id: 5,
-		question: "지나가던 골목에서 불량배가 어린아이의 돈을 뺐는 모습을 보았다. 이때, 당신의 행동은?",
+		question: "지나가던 골목에서 불량배가 어린아이의 돈을 뺐는 모습을 보았다. 이때 당신의 행동은?",
 		answers: [
 			{ text: "직접 가서 도와준다.", combat: 2, supply: 0 },
-			{ text: "경찰에 신고 후 올때까지 기다린다.", combat: 0, supply: 2 },
+			{ text: "경찰에 신고하고 올 때까지 기다린다.", combat: 0, supply: 2 },
 			{ text: "그냥 지나간다.", combat: 1, supply: 1 },
 		],
 	},
