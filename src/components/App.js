01e2e13f35ab5eba3faed1947428e0e7a883f5b9
YWTechIT <abcd8637@naver.com>
2021-05-24 22:03:12 +0900

    Update use Route each page to App.js
    Route를 사용하여 Category, Combat, Supply Page를 App.js로 통합시킴

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,38 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import DEFAULTSCORE from "../constant/defaultScore";
+import getMaxKey from "../utility/getMaxKey";
 import GlobalStyle from "../globalStyle";
-import Quiz from "../pages/Quiz";
+import Category from "../pages/Category";
+import Combat from "../pages/Combat";
+import Supply from "../pages/Supply";
 import theme from "../theme";
 
 const App = () => {
+  const [score, setScore] = useState(DEFAULTSCORE);
+  const categoryKey = getMaxKey(score.category);
+  const combatKey = getMaxKey(score.combat);
+  const supplyKey = getMaxKey(score.supply);
+
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <Quiz></Quiz>
+        <Router>
+          <Route exact path="/">
+            <p>landing Page</p>
+          </Route>
+          <Route path="/category">
+            <Category score={score} setScore={setScore}></Category>
+          </Route>
+          <Route path="/combat">
+            <Combat score={score} setScore={setScore}></Combat>
+          </Route>
+          <Route path="/supply">
+            <Supply score={score} setScore={setScore}></Supply>
+          </Route>
+        </Router>
       </ThemeProvider>
     </>
   );
