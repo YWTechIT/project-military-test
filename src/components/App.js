82ceed033002150de7e81e7ef3f86fde1bafbd45
YWTechIT <abcd8637@naver.com>
2021-05-24 20:53:37 +0900

    MAKE Quiz component for refactoring
    App.js에서 Rougnt 관련 코드를 작성하기 위해 App.js에 있던 기존의 코드를 모두 Quiz 컴포넌트로 옮겼다.

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,14 @@
-import { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "../globalStyle";
-import Category from "../pages/Category";
-import Combat from "../pages/Combat";
-import Supply from "../pages/Supply";
+import Quiz from "../pages/Quiz";
 import theme from "../theme";
-import defaultScore from "./defaultScore";
-import getMaxKey from "../utility/getMaxKey";
 
 const App = () => {
-  const [score, setScore] = useState(defaultScore);
-  const categoryKey = getMaxKey(score.category);
-  const combatKey = getMaxKey(score.combat);
-  const supplyKey = getMaxKey(score.supply);
-
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <Category score={score} setScore={setScore}></Category>
-        <Combat score={score} setScore={setScore}></Combat>
-        <Supply score={score} setScore={setScore}></Supply>
+        <Quiz></Quiz>
       </ThemeProvider>
     </>
   );
