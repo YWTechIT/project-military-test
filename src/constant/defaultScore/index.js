9a2ab2d9b0c15af3d5e720f286c226b479792ad1
YWTechIT <abcd8637@naver.com>
2021-05-24 20:41:29 +0900

    RENAME CONSTANT parameter in defaultScore
    defaultScore를 CONSTANT화 시키면서 대문자로 이름 변경

---
+++ src/constant/defaultScore/index.js
@@ -0,0 +1,25 @@
+// 초기점수
+const DEFAULTSCORE = {
+    category: {
+      combatScore: 0,
+      supplyScore: 0,
+    },
+    combat: {
+      infantryScore: 0,
+      artilleryScore: 0,
+      armorScore: 0,
+      engineerScore: 0,
+      signalScore: 0,
+      intelligenceScore: 0,
+    },
+    supply: {
+      affairScore: 0,
+      medicScore: 0,
+      weaponScore: 0,
+      policeScore: 0,
+      prayScore: 0,
+      bandScore: 0,
+    },
+  };
+  
+  export default DEFAULTSCORE;
