6b97f15cc9bbca59ad718df8ba357399977190e4
YWTechIT <abcd8637@naver.com>
2021-05-24 20:38:00 +0900

    Rename getCode to getMaxKey, Move component folder to utility folder
    기존의 getCode에서 getMaxKey로 이름을 변경하고, 기존 component 폴더에서 utility 폴더로 이동.

---
+++ src/utility/getMaxKey/index.js
@@ -0,0 +1,39 @@
+// 점수 계산하는 함수
+// object example
+// const DEFAULTSCORE = {
+//   category: {
+//     combatScore: 0,
+//     supplyScore: 0,
+//   },
+//   combat: {
+//     infantryScore: 0,
+//     artilleryScore: 0,
+//     armorScore: 0,
+//     engineerScore: 0,
+//     signalScore: 0,
+//     intelligenceScore: 0,
+//   },
+//   supply: {
+//     affairScore: 0,
+//     medicScore: 0,
+//     weaponScore: 0,
+//     policeScore: 0,
+//     prayScore: 0,
+//     bandScore: 0,
+//   },
+// };
+
+const getMaxKey = (dict) => {
+    let maxValue = 0;
+    let maxKey = "";
+
+    for (let dictKey in dict) {
+      if (dict[dictKey] > maxValue) {
+        maxValue = dict[dictKey];
+        maxKey = dictKey;
+      }
+    }
+    return maxKey;
+  };
+
+export default getMaxKey;
