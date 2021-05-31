1f2608a83020619adf89b358f37540d9d6d8b57f
YWTechIT <abcd8637@naver.com>
2021-05-31 14:25:39 +0900

    ADD PropTypes getMaxValueKey utility
    getMaxValueKey의 함수의 parameter는 dict형태임을 PropTypes로 확인한다.

--- src/utility/getMaxKey/index.js
+++ src/utility/getMaxKey/index.js
@@ -1,39 +1,32 @@
-// 점수 계산하는 함수
-// object example
-// const DEFAULT_SCORE = {
-//   category: {
-//     combatScore: 0,
-//     supplyScore: 0,
-//   },
-//   combat: {
-//     infantryScore: 0,
-//     artilleryScore: 0,
-//     armorScore: 0,
-//     engineerScore: 0,
-//     signalScore: 0,
-//     intelligenceScore: 0,
-//   },
-//   supply: {
-//     affairScore: 0,
-//     medicScore: 0,
-//     weaponScore: 0,
-//     policeScore: 0,
-//     prayScore: 0,
-//     bandScore: 0,
-//   },
-// };
+/*
+* Parameter detail of getMaxValueKey 
+*
+* @ param {dict} 
+* @ example
+*  combat = {
+*  infantryScore = 0,
+*  artilleryScore = 0,
+*  ...
+*  }
+*/
+
+import PropTypes from "prop-types";
 
 const getMaxValueKey = (dict) => {
-    let maxValue = 0;
-    let maxKey = "";
+  let maxValue = 0;
+  let maxKey = "";
 
-    for (let dictKey in dict) {
-      if (dict[dictKey] > maxValue) {
-        maxValue = dict[dictKey];
-        maxKey = dictKey;
-      }
+  for (let dictKey in dict) {
+    if (dict[dictKey] > maxValue) {
+      maxValue = dict[dictKey];
+      maxKey = dictKey;
     }
-    return maxKey;
-  };
+  }
+  return maxKey;
+};
+
+getMaxValueKey.PropTypes = {
+  dict: PropTypes.object.isRequired
+};
 
 export default getMaxValueKey;
