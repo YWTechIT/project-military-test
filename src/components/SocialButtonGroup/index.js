73ab51f2dec27f56c13249a90a169fca0dc63241
YWTechIT <abcd8637@naver.com>
2021-06-02 13:35:41 +0900

    ADD URL share, LINE share, FACEBOOK share Button by

--- src/components/SocialButtonGroup/index.js
+++ src/components/SocialButtonGroup/index.js
@@ -1,4 +1,11 @@
 import styled from "styled-components";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  LineShareButton,
+  LineIcon,
+} from "react-share";
 
 const GridContainer = styled.div`
   display: grid;
@@ -8,13 +15,34 @@ const GridContainer = styled.div`
   margin-bottom: 16px;
 `;
 
+const URLShareButton = styled.button`
+  width: 48px;
+  height: 48px;
+  color: white;
+  border-radius: 24px;
+  border: 0px;
+  font-weight: 800;
+  font-size: 18px;
+  cursor: pointer;
+  background-color: ${(props) => props.theme.primaryColor100};
+  &:hover {
+    background-color: ${(props) => props.theme.primaryColor80};
+  }
+`;
+
 const SocialButtonGroup = () => {
+  const currentUrl = window.location.href;
   return (
     <GridContainer>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>
+      <CopyToClipboard text={currentUrl}>
+        <URLShareButton>URL</URLShareButton>
+      </CopyToClipboard>
+      <FacebookShareButton url={currentUrl}>
+        <FacebookIcon size={48} round={true} borderRadius={24}></FacebookIcon>
+      </FacebookShareButton>
+      <LineShareButton url={currentUrl}>
+        <LineIcon size={48} round={true} borderRadius={24}></LineIcon>
+      </LineShareButton>
     </GridContainer>
   );
 };
