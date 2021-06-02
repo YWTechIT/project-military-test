4c9ca7a9523270846e32a3fc49962237ddccbd6c
YWTechIT <abcd8637@naver.com>
2021-06-02 13:19:40 +0900

    ADD cCode contents
    캐릭터별로 contents를 저장함.

--- src/components/ResultSection/index.js
+++ src/components/ResultSection/index.js
@@ -1,31 +1,39 @@
 import React from "react";
 import styled from "styled-components";
+import RESULT from "./result";
 
 const SectionTitle = styled.h1`
-  font-size: 32px;
+  font-size: 64px;
   font-weight: bold;
   margin-top: 0px;
-  margin-bottom: 32px;
+  margin-bottom: 56px;
   text-align: center;
 `;
 
-// const StyledImage = styled.img`
-//   display: block;
-//   border-radius: 16px;
-//   opacity: 95%;
-//   margin-bottom: 4px;
-//   max-width: 100%;
-// `;
+const ResultTitle = styled.div`
+  font-size: 32px;
+  margin-bottom: 56px;
+  text-align: center;
+`;
+
+const StyledImage = styled.img`
+  display: block;
+  border-radius: 16px;
+  opacity: 95%;
+  margin-bottom: 4px;
+  max-width: 100%;
+`;
 
 // const Source = styled.p`
 //   margin-top: 4px;
 // `;
 
-const ResultSection = ({ maxKey }) => {
+const ResultSection = ({ cCode }) => {
   return (
     <>
-      <SectionTitle>당신은 혹시... {maxKey}</SectionTitle>
-      <p>params image</p>
+      <SectionTitle>당신은 혹시?</SectionTitle>
+      <StyledImage src={RESULT[cCode].src} />
+      <ResultTitle>{RESULT[cCode].title}</ResultTitle>
     </>
   );
 };
