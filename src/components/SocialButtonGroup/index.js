fc8d67a01d5798a6b744b7bd8ef39978d10ad886
YWTechIT <abcd8637@naver.com>
2021-06-02 13:46:25 +0900

    ADD Kakao img, Kakao share Button CSS , Kakao share function(Hooks)
    카카오버튼 공유를 위한 카카오 img, 공유 버튼 CSS, 기능 구현 Hooks

--- src/components/SocialButtonGroup/index.js
+++ src/components/SocialButtonGroup/index.js
@@ -1,11 +1,14 @@
 import styled from "styled-components";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { useScript } from "../../hooks";
 import {
   FacebookShareButton,
   FacebookIcon,
   LineShareButton,
   LineIcon,
 } from "react-share";
+import { useEffect } from "react";
+import kakaoLogo from "../../assets/kakao_logo.jpg";
 
 const GridContainer = styled.div`
   display: grid;
@@ -30,13 +33,46 @@ const URLShareButton = styled.button`
   }
 `;
 
+const KakaoShareButton = styled.a`
+  width: 48px;
+  height: 48px;
+  border-radius: 24px;
+  border: 0px;
+  font-weight: 800;
+  font-size: 18px;
+  cursor: pointer;
+`;
+
+const KakaoIcon = styled.img`
+  width: 48px;
+  height: 48px;
+  border-radius: 24px;
+`;
+
 const SocialButtonGroup = () => {
   const currentUrl = window.location.href;
+  const status = useScript("https://developers.kakao.com/sdk/js/kakao.js");
+  const handleKakaoButton = () => {
+    window.Kakao.Link.sendScrap({
+      requestUrl: currentUrl,
+    });
+  };
+  useEffect(() => {
+    if (status === "ready" && window.Kakao) {
+      if (!window.Kakao.isInitialized()) {
+        window.Kakao.init("2ca911f4d06608b5899e39f80a083215");
+      }
+    }
+  }, [status]);
+
   return (
     <GridContainer>
       <CopyToClipboard text={currentUrl}>
         <URLShareButton>URL</URLShareButton>
       </CopyToClipboard>
+      <KakaoShareButton onClick={handleKakaoButton}>
+        <KakaoIcon src={kakaoLogo}></KakaoIcon>
+      </KakaoShareButton>
       <FacebookShareButton url={currentUrl}>
         <FacebookIcon size={48} round={true} borderRadius={24}></FacebookIcon>
       </FacebookShareButton>
