35991b9d8544416a4f565c6bc05c84737e9f3001
YWTechIT <abcd8637@naver.com>
2021-06-02 14:12:29 +0900

    ADD HorizonLine at ResultPage
    수평선 추가

--- src/components/ResultSection/index.js
+++ src/components/ResultSection/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import HorizonLine from "../HorizontalLine";
 import SocialButtonGroup from "../SocialButtonGroup";
 import RESULT from "./result";
 
 const SectionTitle = styled.h1`
-  font-size: 64px;
+  font-size: 48px;
   font-weight: bold;
-  margin-top: 0px;
+  margin-top: 20px;
   margin-bottom: 56px;
   text-align: center;
 `;
@@ -25,17 +26,20 @@ const StyledImage = styled.img`
   max-width: 100%;
 `;
 
-// const Source = styled.p`
-//   margin-top: 4px;
-// `;
+const Line = styled.div`
+  width: 50%
+  height: 100px;
+  border: 3px;
+`;
 
 const ResultSection = ({ cCode }) => {
   return (
     <>
-      <SectionTitle>당신은 혹시?</SectionTitle>
+      <SectionTitle>나와 잘 맞는 병과는?</SectionTitle>
       <StyledImage src={RESULT[cCode].src} />
       <ResultTitle>{RESULT[cCode].title}</ResultTitle>
       <SocialButtonGroup></SocialButtonGroup>
+      <HorizonLine></HorizonLine>
     </>
   );
 };
