ff88469593a1975e8f68cc7b783ab96a30ec9e72
YWTechIT <abcd8637@naver.com>
2021-06-17 20:57:28 +0900

    Change CSS styleLink Component
    a태크 밑줄 제거, 글자 색상변경

--- src/typography/styledLink/index.js
+++ src/typography/styledLink/index.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 const StyledLink = styled(Link)`
   width: 100%;
   text-align: center;
+  text-decoration: none;
+  color: black;
 `;
 
 export default StyledLink;
