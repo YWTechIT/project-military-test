610a734a4e1d6e7b672e5688deabe46dc5a23b71
YWTechIT <abcd8637@naver.com>
2021-07-01 06:24:11 +0900

    Change 카테고리페이지 문제 별 점수 변경

--- src/pages/Category/Constant.js
+++ src/pages/Category/Constant.js
@@ -5,7 +5,7 @@ const CATEGORIZE_QUIZ = [
 		question: "조별과제 중 첫눈에 반한 이성이 있을 때 당신의 선택은?",
 		answers: [
 			{ text: "그(그녀)의 번호를 물어본다.", combat: 2, supply: 0 },
-			{ text: "이성의 SNS를 들어가본다.", combat: 0, supply: 2 },
+			{ text: "이성의 SNS를 들어가본다.", combat: 1, supply: 1 },
 			{ text: "내 주변에 그(그녀)와 아는사람이 있는지 물어본다.", combat: 1, supply: 1},
 			{ text: "인연이라면 언젠간 또 보겠지하며 무심코 지나간다.", combat: 1, supply: 1},
 		],
@@ -15,8 +15,8 @@ const CATEGORIZE_QUIZ = [
 		id: 2,
 		question: "한라산 등반 도중 중간 지점까지 도착했는데 체력이 고갈된 상태다. 이때 당신의 선택은?",
 		answers: [
-			{ text: "여기까지 올라왔는데 정상은 찍어야지!", combat: 3, supply: 0 },
-			{ text: "여기까지 올라온것도 대단한데 그만 내려가자.", combat: 0, supply: 3 },
+			{ text: "여기까지 올라왔는데 정상은 찍어야지!", combat: 2, supply: 0 },
+			{ text: "여기까지 올라온것도 대단한데 그만 내려가자.", combat: 0, supply: 1 },
 		],
 	},
 	{
@@ -26,7 +26,7 @@ const CATEGORIZE_QUIZ = [
 			{ text: "전사", combat: 2, supply: 0 },
 			{ text: "궁수", combat: 1, supply: 1 },
 			{ text: "도적", combat: 1, supply: 1 },
-			{ text: "마법사", combat: 0, supply: 2 },
+			{ text: "마법사", combat: 0, supply: 1 },
 		],
 	},
 	{
@@ -36,7 +36,7 @@ const CATEGORIZE_QUIZ = [
 			{ text: "화를 내며 당장 사장을 불러달라고 한다.", combat: 2, supply: 0},
 			{ text: "괜찮다며 웃으며 넘긴다.", combat: 1, supply: 1},
 			{ text: "주문을 취소하고 식당을 나간다.", combat: 1, supply: 1},
-			{ text: "SNS에서 별점을 1개만 준다.", combat: 0, supply: 2},
+			{ text: "SNS에서 별점을 1개만 준다.", combat: 0, supply: 1},
 		],
 	},
 	{
@@ -44,7 +44,7 @@ const CATEGORIZE_QUIZ = [
 		question: "지나가던 골목에서 불량배가 어린아이의 돈을 뺐는 모습을 보았다. 이때 당신의 행동은?",
 		answers: [
 			{ text: "직접 가서 도와준다.", combat: 2, supply: 0 },
-			{ text: "경찰에 신고하고 올 때까지 기다린다.", combat: 0, supply: 2 },
+			{ text: "경찰에 신고하고 올 때까지 기다린다.", combat: 0, supply: 1 },
 			{ text: "그냥 지나간다.", combat: 1, supply: 1 },
 		],
 	},
